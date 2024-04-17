8c078f3d5ddbf88616a218db53f3da81eae464bb
ATomas <heslo123456789>
2024-04-17 20:17:14 +0200

    up

--- sp-peds/fxmanifest.lua
+++ sp-peds/fxmanifest.lua
@@ -5,7 +5,4 @@ files {
     'meta/*.meta',
 }
 
-data_file 'PED_METADATA_FILE' 'meta/frost.meta'
-data_file 'PED_METADATA_FILE' 'meta/iq.meta'
-data_file 'PED_METADATA_FILE' 'meta/LedgerJoker.meta'
-data_file 'PED_METADATA_FILE' 'meta/Rager.meta'
+data_file 'PED_METADATA_FILE' 'meta/*.meta'
